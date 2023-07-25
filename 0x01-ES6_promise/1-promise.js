ed80c43a8d95ddfd03ee6ddec9fa03d036e09228
AbogeJr <junioraboge@gmail.com>
2023-07-25 23:14:52 +0300

    fixes

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,8 @@
 export default function getFullResponseFromAPI(success) {
   const res = new Promise((resolve, reject) => {
     if (success === true) {
-      resolve({ status: 200, body: 'success' });
-    } else if (success === false) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
       reject(Error('The fake API is not working currently'));
     }
   });
