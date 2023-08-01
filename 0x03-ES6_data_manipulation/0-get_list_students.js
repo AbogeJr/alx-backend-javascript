5ec0cccd5a46e0a4736e9c6efb2f3d42b3dd3d4e
AbogeJr <junioraboge@gmail.com>
2023-08-01 12:31:25 +0300

    fix: typos

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,7 +3,9 @@
  * */
 
 export default function getListStudents() {
-  return [{ id: 1, fistName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, fistName: 'James', location: 'Columbia' },
-    { id: 5, fistName: 'Serena', location: 'San Francisco' }];
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
